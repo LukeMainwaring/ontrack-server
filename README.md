d8c1c8c1aa50b20aa4e651a9d6f6d55e956339cb
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-01-09 18:49:02 -0500

    created separate repo

---
+++ README.md
@@ -0,0 +1,6 @@
+Docker scripts to run:
+\$ docker-compose up
+
+To stop:
+Ctrl + c,
+\$ docker-compose down
---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const db = require('../db');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).send({ error: 'You must be logged in' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  jwt.verify(token, 'MY_SECRET_KEY', async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: 'You must be logged in' });
+    }
+    const { userId } = payload;
+
+    db.query(
+      'SELECT * FROM users where email=?',
+      userId,
+      (error, results, fields) => {
+        if (error) throw error;
+        req.user = results[0];
+        next();
+      }
+    );
+  });
+};
