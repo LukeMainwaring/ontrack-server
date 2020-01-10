6d8ee250b518c5fbeee736e6ece0d97f0215127f
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-01-09 22:44:28 -0500

    test route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+// const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
 const PORT = 8080;
 // const HOST = '0.0.0.0';
@@ -15,11 +15,15 @@ app.use(
   })
 );
 
-app.use(authRoutes);
-app.use(userRoutes);
+// app.use(authRoutes);
+// app.use(userRoutes);
 
 // app.listen(PORT, HOST);
 // console.log(`Running on http://${HOST}:${PORT}`);
+
+app.get('/test', async (req, res) => {
+  res.send('hello, world!');
+});
 
 // app.listen(PORT);
 app.listen(PORT, err => {
